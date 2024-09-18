3e28be6abcb59b2b596bad938b115dc9e7031125
flaxdrop <90156021+flaxdrop@users.noreply.github.com>
2024-09-18 22:29:26 +0200

    Update while.c

--- while.c
+++ while.c
@@ -19,8 +19,7 @@ int i = 0;
 
 int main()
 {
-   // while loop = checks a condition, THEN executes a block of code if condition is true
-   // do while loop = always executes a block of code once, THEN checks a condition
+   
 
    int number = 0;
    int sum = 0;
